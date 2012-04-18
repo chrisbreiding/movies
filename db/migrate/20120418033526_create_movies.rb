65a266dea5cef48ce819ac37e3ce4c4b8e7dea38
Chris Breiding <chrisbreiding@gmail.com>
2012-04-18 17:16:56 -0400

    Created migration but have not run it

--- db/migrate/20120418033526_create_movies.rb
+++ db/migrate/20120418033526_create_movies.rb
@@ -1,6 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
+      t.string :title
+      t.string :rt_title
+      t.boolean :chris
+      t.boolean :sarah
+      t.boolean :shortlist
 
       t.timestamps
     end
