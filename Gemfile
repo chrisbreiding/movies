b3f2c74004efed6161411aad058dc5dfaa4a7691
Chris Breiding <chrisbreiding@gmail.com>
2012-06-03 16:51:54 -0400

    Add capistrano for deployment

--- Gemfile
+++ Gemfile
@@ -2,27 +2,39 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 
-gem 'sqlite3'
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'mysql2'
+end
+
+gem 'rake', '0.9.2.2'
+
+# Authentication
+# gem 'devise'
 
 group :assets do
   # gem 'sass-rails',   '~> 3.2.3'
   # gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer'
+  gem 'therubyracer'
 
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'execjs'
+gem 'therubyracer', require: "v8"
+
 gem 'jquery-rails'
 
+# JS templates
 gem 'ejs'
 
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
 # Deploy with Capistrano
-# gem 'capistrano'
+gem 'capistrano'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+require "bundler/capistrano"
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano" # Load RVM's capistrano plugin.
+
+set :application, "movies"
+set :repository,  "git://github.com/chrisbreiding/movies.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/srv/www/movies.crbapps.com"
+
+role :web, "chrisbreiding.com"                          # Your HTTP server, Apache/etc
+role :app, "chrisbreiding.com"                          # This may be the same as your `Web` server
+role :db,  "chrisbreiding.com", :primary => true        # This is where Rails migrations will run
+
+set :user, "root"
+set :scm_username, "chrisbreiding"
+set :use_sudo, false
+
+set :stages, ["production"]
+set :default_stage, "production"
+
+# set :rvm_ruby_string, "ruby-1.9.3-p125@movies"
+# set :rvm_type, :user
+# set :rvm_type, :system
+
+after 'deploy:symlink', 'deploy:cleanup' # makes sure there's only 3 deployments, deletes the extras
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+after 'deploy:update_code' do
+  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+end
