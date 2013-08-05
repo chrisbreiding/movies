0c84ac448e562a38a080db3e8ca773885787f363
Chris Breiding <chrisbreiding@gmail.com>
2013-08-04 22:31:52 -0400

    cleanup

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,10 +1,8 @@
 module Api
   class MoviesController < ApplicationController
-    # respond_to :json
 
     def index
-      render json: Movie.limit(50)#.as_json
-      # respond_with Movie.limit(50).as_json
+      render json: Movie.limit(50)
       # if params[:genre_id]
       #   respond_with Genre.where(slug: params[:genre_id]).first.movies
       # else
@@ -14,7 +12,6 @@ module Api
 
     def show
       render json: Movie.find(params[:id])
-      # respond_with movie: Movie.find(params[:id])
       # respond_with Movie.includes(:genres).find(params[:id])
     end
 
